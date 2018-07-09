61defa4749e22ceb4b0ba7a32d298de62b189fbf
Sandy Bagga <sbagga@sfu.ca>
2018-07-09 14:43:58 -0700

    eliminated most of nav bugs: padding issue in mobi, top position in ta

--- P02/js/nav.js
+++ P02/js/nav.js
@@ -3,48 +3,42 @@ var searchIsHidden;
 
 
 $(document).ready(function () {
-    if($("#search").css("display") == "none"){
+    if ($("#search").css("display") == "none") {
         searchIsHidden = true;
     } else {
         searchIsHidden = false;
     }
     $("#menu").click(function () {
-        $("#nav-list").toggleClass("show-menu");
+
         if ($("#menu img").attr("src") == "img/menu.svg") {
             $("#menu img").attr("src", "img/close.svg");
         } else {
             $("#menu img").attr("src", "img/menu.svg");
         }
 
-        if(searchIsHidden){
+        if (!searchIsHidden) {
+            $("#nav-list").toggleClass("show-menu");
+        }
+
+        if (searchIsHidden) {
+            // $("#nav-list").toggleClass("show-menu");
             $("#nav-list").toggleClass("full-mobile-menu");
-            $("#menu").toggleClass("fix");
+            $("#menu").toggleClass("mobi-fix");
         }
-        
+
     });
-    $(window).resize(function(){
-        //window size where the menu button is hidden
-        if ($("#menu").css("display") == "none") {
-            $("#nav-list").removeClass("show-menu");
-        } else if($("#menu img").attr("src") == "img/close.svg"){
-            $("#nav-list").addClass("show-menu");
-        }
-        
-        
-        if($("#search").css("display") == "none"){
+    $(window).resize(function () {
+
+        //menu will reset on resize
+        $("#nav-list").removeClass("show-menu");
+        $("#nav-list").removeClass("full-mobile-menu");
+        $("#menu").removeClass("mobi-fix");
+        $("#menu img").attr("src", "img/menu.svg");
+
+        if ($("#search").css("display") == "none") {
             searchIsHidden = true;
         } else {
             searchIsHidden = false;
         }
-       
-        if(!searchIsHidden) {
-            $("#nav-list").removeClass("full-mobile-menu");
-            $("#menu").removeClass("fix");
-        } else if (searchIsHidden && $("#menu img").attr("src") == "img/close.svg"){
-            $("#nav-list").addClass("full-mobile-menu");
-            $("#menu").addClass("fix");
-        }
-        
     });
 });
-
